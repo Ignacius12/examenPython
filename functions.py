cd8c6b209b46e43081bdb04badfefda087be9ce9
Ignacio <ignacioya2002@gmail.com>
2023-03-22 10:46:14 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -1,9 +1,25 @@
+
 def split(diccionario):
     white = {}
     red = {}
     for i in diccionario:
-        if diccionario["type"]==white:
+        if i["type"]==white:
             white.update(diccionario[i])
-    
+        if i["type"]==red:
+            red.update(diccionario[i])    
+    for i in red:
+        i.pop("type")
+    for i in white:
+        i.pop("type")    
+    return white,red
 
-    return
+
+def reduce(diccionario,clave):
+    resp = []
+    for i in diccionario:
+        for j in i:
+            if j.get(clave)!=None:
+                resp.append(j[clave])
+            else:
+                return    
+
